225c23efb43bc5379e8a74ab87dd6d137b6cee42
JithinPanicker <jithinck2000@gmail.com>
2026-02-05 23:51:07 +0530

    payment page fixed

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "astro-manager-v19"; // INCREMENT THIS EVERY TIME YOU CHANGE CODE
+const CACHE_NAME = "astro-manager-v20"; // INCREMENT THIS EVERY TIME YOU CHANGE CODE
 const ASSETS_TO_CACHE = [
     "./",
     "./index.html",
