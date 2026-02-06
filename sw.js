44b127c22a562089fb94c0f8dad2493669cd52c4
JithinPanicker <jithinck2000@gmail.com>
2026-02-06 23:30:13 +0530

    removed the pdf auto text

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "astro-manager-v29"; // INCREMENT THIS EVERY TIME YOU CHANGE CODE
+const CACHE_NAME = "astro-manager-v30"; // INCREMENT THIS EVERY TIME YOU CHANGE CODE
 const ASSETS_TO_CACHE = [
     "./",
     "./index.html",
